c79e82ba126c9248b509a25253b147170691a089
Kyrubi <u20221c486@upc.edu.pe>
2025-05-02 12:42:32 -0500

    feat(main): implement main view structure and behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './public/pages/home/home.component';
+import {TranslateService} from '@ngx-translate/core';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatAnchor} from '@angular/material/button';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HomeComponent
+    MatToolbar,
+    MatToolbarRow,
+    RouterLink,
+    LanguageSwitcherComponent,
+    MatAnchor
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learning-center';
+  options = [
+    {path: '/home', title: 'Home'},
+    {path: '/about', title: 'About'}
+  ]
+
+  constructor (private translate: TranslateService) {
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
